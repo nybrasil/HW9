bf6b7556d3e8f647968356d8e4e1e4fa1d24dd85
Tania Bezerra <nybrasil@gmail.com>
2024-03-21 12:31:36 -0400

    created a funtion to write README file

--- index.js
+++ index.js
@@ -20,7 +20,17 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`File ${fileName} has been created successfully.`);
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
